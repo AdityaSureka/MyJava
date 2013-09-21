711d2f27e9be05f450d00619f659cb1fc6d6fdd7
Aditya Sureka <adityasureka92@gmail.com>
2013-09-22 00:40:39 +0530

    Necessary Comments

--- program15.java
+++ program15.java
@@ -1,25 +1,28 @@
  /* LCM using loop
   * 
   * by Aditya Sureka
-  */import java.io.*;
-    public class program15
-    {
-      public static void main(String args[])throws IOException
-      {   
-    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Enter the two nos.");
-    int big=Integer.parseInt(br.readLine());
-    int small=Integer.parseInt(br.readLine());
-    int rem=1;
-   while(rem!=0)
-    {
+  */
+  //import the IO package
+  import java.io.*;
+  public class program15  //class name
+  {
+    public static void main(String args[])throws IOException ///main function
+   {   
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     System.out.println("Enter the two nos.");
+     //Accept the inputs from the user
+     int big=Integer.parseInt(br.readLine());
+     int small=Integer.parseInt(br.readLine());
+     int rem=1;
+     while(rem!=0)
+      {
         rem=big%small;
         if(rem!=0)
-        {
+         {
             big=small;
             small=rem;
-
-        }
-    }
+          }
+      }
     System.out.println(small);
-    }}
+    }
+  }
